ca214b73468a3c02531a2849b1f3e27ff60c6272
Pablo Vinicius Pereira De Sa <pablo.sa@linx.com.br>
2020-03-25 10:09:09 -0300

    Testes

---
+++ src/test/java/com/Agenda/apirest/EndPoints/OperadoraServiceTest.java
@@ -0,0 +1,38 @@
+package com.Agenda.apirest.EndPoints;
+
+import java.math.BigDecimal;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.Agenda.apirest.entity.Operadora;
+import com.Agenda.apirest.services.OperadoraService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+public class OperadoraServiceTest {
+
+	@Autowired
+	OperadoraService operadoraService;
+	
+	@Test
+	void contextLoads() {
+
+	}
+
+
+	public void save() {
+		Operadora operadoratest = new Operadora("Algar", new Long(24), "Móvel", new BigDecimal(65.5));
+		Operadora operadoraDTO = operadoraService.save(operadoratest);
+		Assertions.assertThat(operadoraDTO.getNome()).isNotEmpty();
+		Assertions.assertThat(operadoraDTO.getCodigo()).isNotNull();
+		Assertions.assertThat(operadoraDTO.getCategoria()).isEqualTo("Móvel");
+		Assertions.assertThat(operadoraDTO.getPreco()).isNotNull();
+	}
+
+}
